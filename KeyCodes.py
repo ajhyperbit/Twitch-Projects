0703442c69e7c93701fe0fd47650560a599da688
ajhyperbit <ajhyperbit@gmail.com>
2025-12-17 20:55:41 -0600

    Add comment that points more definitively to the original codebase of the file

--- KeyCodes.py
+++ KeyCodes.py
@@ -1,6 +1,7 @@
 # DougDoug Note: 
 # This code contains key codes plus functions to press keys on Windows
 # You should not need to modify anything in this file, just use as is.
+#Original Codebase:
 #https://github.com/DougDougGithub/TwitchPlays/blob/main/TwitchPlays_KeyCodes.py
 
 import time
